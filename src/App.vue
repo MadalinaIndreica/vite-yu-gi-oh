89586508aec203ba281c29718d0f6dde105bf15b
MadalinaIndreica <madalinaindreicamaddy4@gmail.com>
2023-11-27 16:33:35 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+
+import axios from "axios";
+import { store } from "./store.js";
+import AppHeader from "./components/AppHeader.vue";
+import AppCardList from "./components/AppCardList.vue";
+export default {
+  data() {
+    return {
+      
+      store,
+    };
+  },
+  created() {
+    
+    axios.get(this.store.apiUrl).then((resp) => {
+      console.log(resp);
+      this.store.cards = resp.data.data;
+      
+    });
+  },
+  components: { AppHeader, AppCardList },
+};
+
+
+</script>
+
+<template>
+   <AppHeader />
+   <AppCardList />
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+
+</style>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    props: {
+    card: Object,
+  },
+
+}
+
+
+</script>
+
+<template>
+
+     <div>
+    <img :src="card.card_images[0].image_url" alt="" />
+    <h3>{{ card.name }}</h3>
+
+  </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
